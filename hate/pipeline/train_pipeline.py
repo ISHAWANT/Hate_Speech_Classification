36a61df136b912e64f619c475209c5545b3d1c68
ishawant kumar <ishawantkumar@gmail.com>
2024-03-03 19:39:36 +0530

    data ingestion completed

--- hate/pipeline/train_pipeline.py
+++ hate/pipeline/train_pipeline.py
@@ -23,3 +23,13 @@ class TrainPipeline:
 
         except Exception as e:
             raise CustomException(e, sys) from e
+        
+    def run_pipeline(self) -> None:
+        logging.info("Entered the run_pipeline method of TrainPipeline class")
+        try:
+            data_ingestion_artifacts = self.start_data_ingestion()  
+
+            logging.info("Exited the run_pipeline method of TrainPipeline class")
+
+        except Exception as e:
+            raise CustomException(e, sys) from e 
