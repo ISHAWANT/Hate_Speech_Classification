9b0d3edd7feadbeb64bad03c8ace331e41a72737
ishawant kumar <ishawantkumar@gmail.com>
2024-03-03 08:16:08 +0530

    setup.py updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages,setup 
+setup(
+    name='hate-speech_classification',
+    version='0.0.1',
+    author='ishawant_kumar',
+    author_email='ishawantkumar@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
