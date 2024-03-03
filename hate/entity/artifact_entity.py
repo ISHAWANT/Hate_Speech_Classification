2ee20f7ac5e4bb2efff7246d0ace7e711b6e787f
ishawant kumar <ishawantkumar@gmail.com>
2024-03-03 19:28:41 +0530

    data ingestion updated

--- hate/entity/artifact_entity.py
+++ hate/entity/artifact_entity.py
@@ -1,7 +1,22 @@
 from dataclasses import dataclass
 
-# Data ingestion artifacts
 @dataclass
 class DataIngestionArtifacts:
-    imbalance_data_file_path: str
-    raw_data_file_path: str
+    """
+    Represents the artifacts generated during the data ingestion process.
+
+    Attributes:
+        imbalance_data_file_path (str): The file path to the imbalance data.
+        raw_data_file_path (str): The file path to the raw data.
+    """
+
+    def __init__(self, imbalance_data_file_path: str, raw_data_file_path: str):
+        """
+        Initializes the DataIngestionArtifacts object.
+
+        Args:
+            imbalance_data_file_path (str): The file path to the imbalance data.
+            raw_data_file_path (str): The file path to the raw data.
+        """
+        self.imbalance_data_file_path = imbalance_data_file_path
+        self.raw_data_file_path = raw_data_file_path
--- hate/entity/config_entity.py
+++ hate/entity/config_entity.py
@@ -1,14 +1,30 @@
 from dataclasses import dataclass
-from hate.constants import *
-import os 
+import os
+from hate.constants import * 
 
 @dataclass
 class DataIngestionConfig:
+    """
+    Configuration class for data ingestion.
+
+    Attributes:
+        BUCKET_NAME (str): The name of the bucket where data is stored.
+        ZIP_FILE_NAME (str): The name of the zip file containing the data.
+        DATA_INGESTION_ARTIFACTS_DIR (str): Directory for storing data ingestion artifacts.
+        DATA_ARTIFACTS_DIR (str): Directory for storing data artifacts.
+        NEW_DATA_ARTIFACTS_DIR (str): Directory for storing new data artifacts.
+        ZIP_FILE_DIR (str): Directory for the zip file.
+        ZIP_FILE_PATH (str): Full path to the zip file.
+    """
+
     def __init__(self):
+        # Setting default values for attributes
         self.BUCKET_NAME:str = BUCKET_NAME
         self.ZIP_FILE_NAME: str = ZIP_FILE_NAME
-        self.DATA_INGESTION_ARTIFACTS_DIR: str = os.path.join(os.getcwd(),ARTIFACTS_DIR,DATA_INGESTION_ARTIFACTS_DIR)
-        self.DATA_ARTIFACTS_DIR: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,DATA_INGESTION_IMBALANCE_DATA_DIR)
-        self.NEW_DATA_ARTIFACTS_DIR: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,DATA_INGESTION_RAW_DATA_DIR)
+
+        # Constructing directory paths based on current working directory and constants
+        self.DATA_INGESTION_ARTIFACTS_DIR: str = os.path.join(os.getcwd(), ARTIFACTS_DIR, DATA_INGESTION_ARTIFACTS_DIR)
+        self.DATA_ARTIFACTS_DIR: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR, DATA_INGESTION_IMBALANCE_DATA_DIR)
+        self.NEW_DATA_ARTIFACTS_DIR: str = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR, DATA_INGESTION_RAW_DATA_DIR)
         self.ZIP_FILE_DIR = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR)
-        self.ZIP_FILE_PATH = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR,self.ZIP_FILE_NAME)
+        self.ZIP_FILE_PATH = os.path.join(self.DATA_INGESTION_ARTIFACTS_DIR, self.ZIP_FILE_NAME)
